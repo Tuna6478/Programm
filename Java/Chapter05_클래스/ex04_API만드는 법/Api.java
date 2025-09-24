190f9194775cb6914c03b1597440d1fbb15e8f65
Tuna6478 <minwoo6478@gmail.com>
2025-09-24 09:33:20 +0900

    Add files via upload

---
+++ Java/Chapter05_클래스/ex04_API만드는 법/Api.java
@@ -0,0 +1,18 @@
+package Chapter05.ex04;
+
+public class Api {
+		/*
+		 * 이 메서드는 a 와 b 를 넣으면 + 한 값을 반환하는 <b>엄청난</b> 메서드 입니다.
+		 * 만든이 : 홀길동 (MyApi33@gmai.com)
+		 * @param a 더할 첫번째 수
+		 * @param b 더할 두번째 수
+		 * @return a와 b 가 더해진 결과값
+		*/
+		
+//	   [반환 타입] [method name] [매개변수]  매개변수 : 들어오는 타입
+		int plus(int a, int b) {
+			return a+b;
+		}
+		
+
+	}
