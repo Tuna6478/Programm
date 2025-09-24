93ed7b11b4e97af7d4969e4962f920a92159cc87
Tuna6478 <minwoo6478@gmail.com>
2025-09-24 09:35:37 +0900

    생성자

---
+++ Java/ex05_생성자/Robot.java
@@ -0,0 +1,14 @@
+package Chapter05.ex05;
+
+public class Robot {
+
+	public Robot() { // 생성자 ( 메서드 처럼 생겼지만 반환 타입이 없음 )
+//		생성자는 당연한거라 작성하지 않아도 자동으로 실행이 된다.
+//		객체화 할때 역활을 하게 된다. 
+		System.out.println("로봇이 객체화 하려면 이게 실행된다.");
+		
+	}
+	
+//	 - 생성자 - 
+
+}
